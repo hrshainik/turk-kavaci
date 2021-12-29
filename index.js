310a8bc63685febf68a8ff13c56696b70f4bbbdd
Habibur Rahman <hrsshainik@gmail.com>
2021-12-29 21:55:40 +0600

    slider animation

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-const navSlide= ()=>{
-    const burGer = document.querySelector(".burger");
-    const navLinks = document.querySelector(".items");
+const navSlide = () => {
+  const burGer = document.querySelector(".burger");
+  const navLinks = document.querySelector(".items");
 
-burGer.addEventListener("click",() =>{
-    navLinks.classList.toggle("nav-active")
-});
+  burGer.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+  });
 
-navLinks.addEventListener("click", () => {
-    navLinks.classList.toggle("nav-active")
-})
-}
+  navLinks.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+  });
+};
 
 navSlide();
+
+const interleaveOffset = 0.75;
+
+const swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  speed: 900,
+  mousewheelControl: true,
+  watchSlidesProgress: true,
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+    type: "bullets",
+    renderBullet: function (index, className) {
+      return (
+        '<span class="' + className + '">' + ("0" + (index + 1)) + "</span>"
+      );
+    },
+  },
+  on: {
+    progress: function () {
+      console.log("test");
+      let swiper = this;
+
+      for (let i = 0; i < swiper.slides.length; i++) {
+        let slideProgress = swiper.slides[i].progress;
+        let innerOffset = swiper.height * interleaveOffset;
+        let innerTranslate = slideProgress * innerOffset;
+
+        TweenMax.set(swiper.slides[i].querySelector(".slide-inner"), {
+          y: innerTranslate,
+        });
+      }
+    },
+    setTransition: function (slider, speed) {
+      let swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".slide-inner").style.transition =
+          speed + "ms";
+      }
+    },
+  },
+});
